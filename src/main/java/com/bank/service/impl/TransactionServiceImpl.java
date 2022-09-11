d2bdd8ea8ce5f636c726863fa309acfbe11b0147
civanozbay <civan417@hotmail.com>
2022-09-11 14:34:27 +0200

    Initial commit

---
+++ src/main/java/com/bank/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,90 @@
+package com.bank.service.impl;
+
+import com.bank.enums.AccountType;
+import com.bank.exception.AccountOwnershipException;
+import com.bank.exception.BadRequestException;
+import com.bank.exception.BalanceNotSufficientException;
+import com.bank.model.Account;
+import com.bank.model.Transaction;
+import com.bank.repository.AccountRepository;
+import com.bank.service.TransactionService;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+@Component
+public class TransactionServiceImpl implements TransactionService {
+    AccountRepository accountRepository;
+
+    public TransactionServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creationDate, String message) {
+        
+        validateAccount(sender,receiver);
+        checkAccountOwnership(sender,receiver);
+        executeBalanceAndUpdateIfRequired(amount,sender,receiver);
+                /*
+        after all validations are completed, and money is transferred, we need to create Transaction object and save/return it
+         */
+        //please create needed classes/ methods for this step, save the transactions.
+        return null;
+    }
+
+    private void executeBalanceAndUpdateIfRequired(BigDecimal amount, Account sender, Account receiver) {
+        if(checkSenderBalance(sender,amount)){
+            sender.setBalance(sender.getBalance().subtract(amount));
+            receiver.setBalance(receiver.getBalance().add(amount));
+        }else{
+            throw new BalanceNotSufficientException("Balance is not enough for this transfer");
+        }
+    }
+    private boolean checkSenderBalance(Account sender, BigDecimal amount) {
+        // verify that sender has enough balance
+        return sender.getBalance().subtract(amount).compareTo(BigDecimal.ZERO) >=0;
+    }
+    private void checkAccountOwnership(Account sender, Account receiver) {
+        /*
+         write an if statement that checks if one of the account is saving,
+         and user if of sender or receiver is not the same, throw AccountOwnershipException
+         */
+
+        if((sender.getAccountType().equals(AccountType.SAVING)
+                ||receiver.getAccountType().equals(AccountType.SAVING))&& !sender.getUserId().equals(receiver.getUserId()))
+        {
+            throw new AccountOwnershipException("If one of the account is saving, userId must be the same");
+        }
+    }
+
+    private void validateAccount(Account sender, Account receiver) {
+        /*
+        if any of the account is null
+        if account ids are the same (same account)
+        if the account exist in the database(repository)
+         */
+
+        if(sender==null || receiver==null){
+            throw new BadRequestException("Sender or Receiver cannot be null");
+        }
+        if(sender.getId().equals(receiver.getId())){
+            throw new BadRequestException("Sender accounts needs to be different than receiver");
+        }
+
+        findAccountById(sender.getId());
+        findAccountById(receiver.getId());
+        
+    }
+
+    private void findAccountById(UUID id) {
+        accountRepository.findBy(id);
+    }
+
+    @Override
+    public List<Transaction> findAllTransaction() {
+        return null;
+    }
+}
