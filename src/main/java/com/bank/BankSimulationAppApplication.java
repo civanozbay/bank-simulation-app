feb374d5e1280efcbf3fa95d908e0969a37723fb
civanozbay <civan417@hotmail.com>
2022-09-12 16:45:16 +0200

    app spring boot part is completed

--- src/main/java/com/bank/BankSimulationAppApplication.java
+++ src/main/java/com/bank/BankSimulationAppApplication.java
@@ -1,13 +1,39 @@
 package com.bank;
 
+import com.bank.enums.AccountType;
+import com.bank.model.Account;
+import com.bank.repository.AccountRepository;
+import com.bank.repository.TransactionRepository;
+import com.bank.service.AccountService;
+import com.bank.service.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankSimulationAppApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(BankSimulationAppApplication.class, args);
+
+        //get account and transaction service beans
+        AccountService accountService = context.getBean(AccountService.class);
+        TransactionService transactionService = context.getBean(TransactionService.class);
+
+        // create 2 account sender and receiver.
+        Account sender = accountService.createNewAccount(BigDecimal.valueOf(100),new Date(), AccountType.CHECKING,1L);
+        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.SAVING,1L);
+        accountService.listAllAcount().forEach(System.out::println);
+        transactionService.makeTransfer(sender,receiver,new BigDecimal(90),new Date(),"Transaction-1");
+
+        System.out.println(transactionService.findAllTransaction().get(0));
+        accountService.listAllAcount().forEach(System.out::println);
+
     }
 
 }
--- src/main/java/com/bank/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/bank/service/impl/TransactionServiceImpl.java
@@ -4,10 +4,13 @@ import com.bank.enums.AccountType;
 import com.bank.exception.AccountOwnershipException;
 import com.bank.exception.BadRequestException;
 import com.bank.exception.BalanceNotSufficientException;
+import com.bank.exception.UnderConstructionException;
 import com.bank.model.Account;
 import com.bank.model.Transaction;
 import com.bank.repository.AccountRepository;
+import com.bank.repository.TransactionRepository;
 import com.bank.service.TransactionService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -16,23 +19,32 @@ import java.util.List;
 import java.util.UUID;
 @Component
 public class TransactionServiceImpl implements TransactionService {
+    @Value("${under_construction}")
+    private boolean underConstruction;
     AccountRepository accountRepository;
+    TransactionRepository transactionRepository;
 
-    public TransactionServiceImpl(AccountRepository accountRepository) {
+    public TransactionServiceImpl(AccountRepository accountRepository, TransactionRepository transactionRepository) {
         this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
     }
 
     @Override
     public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creationDate, String message) {
-        
-        validateAccount(sender,receiver);
-        checkAccountOwnership(sender,receiver);
-        executeBalanceAndUpdateIfRequired(amount,sender,receiver);
+        if (!underConstruction) {
+            validateAccount(sender, receiver);
+            checkAccountOwnership(sender, receiver);
+            executeBalanceAndUpdateIfRequired(amount, sender, receiver);
                 /*
         after all validations are completed, and money is transferred, we need to create Transaction object and save/return it
          */
-        //please create needed classes/ methods for this step, save the transactions.
-        return null;
+            //please create needed classes/ methods for this step, save the transactions.
+            Transaction transaction = Transaction.builder().amount(amount).sender(sender.getId())
+                    .receiver(receiver.getId()).creationDate(creationDate).message(message).build();
+            return transactionRepository.save(transaction);
+        }else {
+            throw new UnderConstructionException("App is under construction, try again later");
+        }
     }
 
     private void executeBalanceAndUpdateIfRequired(BigDecimal amount, Account sender, Account receiver) {
@@ -85,6 +97,6 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public List<Transaction> findAllTransaction() {
-        return null;
+        return transactionRepository.findAll();
     }
 }
